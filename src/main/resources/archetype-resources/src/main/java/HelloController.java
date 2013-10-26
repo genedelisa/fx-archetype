4f6e7963927ccc230f789274351c1608e2dc0c31
Gene De Lisa <gene@rockhoppertech.com>
2013-10-26 13:32:15 -0400

    Initial

---
+++ src/main/resources/archetype-resources/src/main/java/HelloController.java
@@ -0,0 +1,30 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloController {
+    private static final Logger logger = LoggerFactory
+            .getLogger(HelloController.class);
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+        logger.debug("initializing");
+
+    }
+
+}
